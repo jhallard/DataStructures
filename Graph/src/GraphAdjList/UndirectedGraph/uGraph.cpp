f5cdac5405c229f0f574b3a1e39864b27d95cf27
jhallard <jhallard@ucsc.edu>
2014-11-11 15:17:44 -0800

    Performing a small amount of testing, adding lot's of inline code documentation

--- Graph/src/GraphAdjList/UndirectedGraph/uGraph.cpp
+++ Graph/src/GraphAdjList/UndirectedGraph/uGraph.cpp
@@ -2,6 +2,7 @@
 *   @Author   - John H Allard Jr.
 *   @File     - uGraph.cpp
 *   @Data     - 11/12/2014
+*	@Repo     - https://github.com/jhallad/DataStructures/Graph
 *   @Purpose  - This is my implementation of the uGraph (undirected Graph) class defined in uGraph.h. This class represents a templated, 
 *				undirected graph, upon which a user can perform operation like searches, path finding, and other things. 
 *				This class was implemented as part of my C++. Data Structures personal project. All code is open license and free to use.
@@ -11,8 +12,23 @@
 *				if some vertex v had edges connected it with vertices a, b, c, d, and e, the adj list for vertex v would look like
 *				v -> a* -> b* -> c* -> d* -> null
 *				Where x* is an edge that leads from vertex v to vertex x.
-*				This above is just one adjacency list, our graph will have a single adjacency list for each vertex in the graph. So 
-
+*				This above is just one adjacency list, our graph will have a single adjacency list for each vertex in the graph. So our graph data structure
+*				will look more like this :
+*				
+*				Vertices -> List of Edges 
+*				-----------------------------------
+*			  1 |   a  -> c* -> d* -> null        |
+*			  2 |   b  -> v* -> d* -> e* -> null  |
+*			  3 |   c  -> a* -> v* -> e* -> null  |
+*			  4 |   d  -> a* -> b* -> null        | 
+*			  5 |   e  -> b* -> c* -> v* -> null  |
+*			  6 |   v  -> b* -> c* -> null        |
+*				-----------------------------------
+*
+*				This class inherits from the pure, virtual GraphInterface class (GraphInterface/GraphInterface.h). This interface specifies exactly what functions
+*				both the undirected and directed graph classes that I make must publically implement. This is done to help ensure that the all user interaction
+*				with the graph in well planned out, consistent, and doesn't derive itself in any way from the implementation details of the graph. This should allow
+*				me to make amny different representations of graphs (adjlist's, adjmatrices, etc.) that can all be used in the exact same way by the user.
 **/
 
 #include "uGraph.h"
@@ -140,7 +156,7 @@ bool uGraph<VertexType>::deleteEdge(VertexType v1, VertexType v2) {
 
 
 // @func   - getWeight
-// @args   - #1 From node, #2 "To" Node
+// @args   - #1 One Vertex of the edge in question, #2 The other vertex of teh edge in question
 // @return - Boolean indicating succes 
 template<class VertexType>
 double uGraph<VertexType>::getWeight(VertexType v1, VertexType v2)  {
